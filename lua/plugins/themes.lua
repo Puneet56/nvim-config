ac1bba73ed63c0fde2c9d748bfd385e8b7c63723
= <puneet@Puneets-MacBook-Air.local>
2023-12-19 22:10:55 +0530

    made nvim transparent

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,5 +1,13 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", opts = {
+    transparent_mode = true,
+  } },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
